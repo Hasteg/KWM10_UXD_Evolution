1f4ab3b398e4088ec9c7c813d563c04889dd99f5
Stefan Habring <habring.stefan@gmail.com>
2013-06-03 18:51:14 +0200

    Added Hs

--- js/main.js
+++ js/main.js
@@ -8,9 +8,17 @@ $(function () {
     //            edgeStrategy: 'set',
 		constants: {
 		    h: h,
-                    h2: h*2,
-                    h3: h*3,
-                    h4: h*4
+            h2: h*2,
+            h3: h*3,
+            h4: h*4,
+            h5: h*5,
+            h6: h*6,
+            h7: h*7,
+            h8: h*8,
+            h9: h*9,
+            h10: h*10,
+            h11: h*11,
+            h12: h*12
 		},
 		easing: {
 		    stepper: function (p) {
